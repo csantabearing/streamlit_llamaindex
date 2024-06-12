ce263a803d7254991f60ef848200d91ca5aaa41b
Camilo Santa <61987046+csantabearing@users.noreply.github.com>
2024-06-12 16:52:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,47 @@
-from openai import OpenAI
 import streamlit as st
-import os
+import openai
+from llama_index.llms.openai import OpenAI
+try:
+  from llama_index import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
+except ImportError:
+  from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
 
-st.title("ChatGPT-like clone")
+st.set_page_config(page_title="Chat with Camilo Santa", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+openai.api_key = os.environ['OPENAI_APIKEY']
+st.title("Chat with Camilo Santa")
+st.info("Check the full code in [github](https://github.com/csantabearing/cloudrun_python)", icon="📃")
+         
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about my CV!"}
+    ]
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading CV..."):
+        system_prompt="You are Camilo Santa, you're in a tech interview. Assume all questions are related to your work experience. Keep your answers based on facts – do not hallucinate."
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt=system_prompt))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+index = load_data()
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+if "chat_engine" not in st.session_state.keys():
+    st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+if prompt := st.chat_input("Your question"): 
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+        st.write(message["content"])
 
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
+if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message)
