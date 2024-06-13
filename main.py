c330ac7282370d1c1577e17d0a9b39571fb4153d
csantabearing <camilo@bearing.ai>
2024-06-13 09:08:22 -0500

    config

--- main.py
+++ main.py
@@ -5,22 +5,23 @@ try:
   from llama_index import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
 except ImportError:
   from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
+import json
 import os
 
-st.set_page_config(page_title="Chat with Camilo Santa", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+config=json.load(open('./data/config.json'))
+st.set_page_config(page_title=config['title'], page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = os.getenv("OPENAI_API_KEY")
-st.title("Chat with Camilo Santa")
-st.info("Check the full code in [github](https://github.com/csantabearing/cloudrun_python)", icon="📃")
-         
+st.title(config['subtitle'])
+st.info(config['info'], icon="📃")
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about my CV!"}
+        {"role": "assistant", "content": config['initial']}
     ]
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading CV..."):
-        system_prompt="You are Camilo Santa, you're in a tech interview. Assume all questions are related to your work experience. Keep your answers based on facts – do not hallucinate."
+    with st.spinner(text=config['spinner']):
+        system_prompt=config['prompt']
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt=system_prompt))
@@ -32,7 +33,7 @@ index = load_data()
 if "chat_engine" not in st.session_state.keys():
     st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-if prompt := st.chat_input("Your question"): 
+if prompt := st.chat_input(config['chatprompt']):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
 for message in st.session_state.messages:
@@ -41,7 +42,7 @@ for message in st.session_state.messages:
 
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
+        with st.spinner(config['spinner']):
             response = st.session_state.chat_engine.chat(prompt)
             st.write(response.response)
             message = {"role": "assistant", "content": response.response}
